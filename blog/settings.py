567e0afab20ea48ca19583b22f4259753ee35885
Israel <israeloloruntoba3@email.com>
2025-05-24 08:47:49 +0100

    Fix static files and update templates

--- blog/settings.py
+++ blog/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
@@ -128,6 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Media files (Uploaded files)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_ROOT = BASE_DIR / 'media'  # Ensure this directory exists
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
